95a33ae0ff2a399f1974d5481a4577697ac58855
Lewis Daly <lewis.daly@enabled.com.au>
2018-02-12 09:01:00 +1030

    work on stupid image issue

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/gb-web
 npm run build
 
+
 cd ..
+mkdir -p public/images
+cp gb-web/src/images/* public/images
 firebase deploy --only hosting
